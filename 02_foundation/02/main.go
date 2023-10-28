1f7211b882e056812198d7ee5090d16951eb4f63
Vagner Kuntz <vaagner.kuntz@gmail.com>
2023-10-28 10:49:23 -0300

    declaration and attribuition

---
+++ 02_foundation/02/main.go
@@ -0,0 +1,18 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool = true
+	c int
+	d string
+	e float64 = 1.2
+)
+
+func main() {
+	// var a string = "x"
+	a := "x"
+	a = "y"
+
+	println(a)
+}
